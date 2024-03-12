6357ded4c7c7cddaa0ec7c992d7bd3f50f1d25b8
Atul Aryan <imatularyan@gmail.com>
2024-03-12 18:00:57 +0530

    basic features added

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const articleRouter = require("./routes/articles");
+const Article = require("./models/article");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 3000;
+const app = express();
+
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+const databaseName = "blogDB";
+
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@cluster0.cklpl3g.mongodb.net/${databaseName}`
+);
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: articles });
+});
+
+app.use("/articles", articleRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost${port}`);
+});
